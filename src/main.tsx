e7fd623eb13b3091d502e5fac52aa762cdf67a38
Derekp8 <derekphinney44@gmail.com>
2026-08-21 13:27:54 -0500

    feat: mount strict manual product surface

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./ui/App";
+import M15StrictManualSurface from "./ui/m15-strict-manual-surface";
 import "./ui/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <M15StrictManualSurface />
   </StrictMode>,
 );
