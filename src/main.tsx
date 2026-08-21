ab39a1b587fc41e380f7e93993ed513e0bb668a8
Derekp8 <derekphinney44@gmail.com>
2026-08-21 16:16:45 -0500

    refactor: remove strict manual DOM sidecar

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./ui/App";
-import M15StrictManualSurface from "./ui/m15-strict-manual-surface";
 import "./ui/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <M15StrictManualSurface />
   </StrictMode>,
 );
